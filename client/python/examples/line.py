9776160a0469b2221e86fd547429ca89e0074259
Folkert van Heusden <folkert@vanheusden.com>
2026-01-12 18:12:24 +0100

    work on examples

---
+++ client/python/examples/line.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+import backend_ddp
+import frontend
+import random
+import time
+
+
+canvas = frontend.frontend(backend_ddp.backend_ddp('192.168.65.140', 4048, (64, 32)))
+width, height = canvas.get_resolution()
+canvas.clear_screen()
+for y in range(height):
+    canvas.draw_line_rgb(0, y, width, y, 0, 0, 255)
+canvas.draw_line_rgb(0, 0, width, height, 0, 255, 0)
+canvas.draw_line_rgb(width, 0, 0, height, 255, 0, 0)
+canvas.send_to_screen()
+
+while True:
+    time.sleep(3600)
