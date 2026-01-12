c1a3297823911a82aa954f907a2800dd96e85c92
Folkert van Heusden <folkert@vanheusden.com>
2026-01-12 18:11:06 +0100

    ddp instead of pixelflood target

--- client/python/examples/scroller.py
+++ client/python/examples/scroller.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
 
-import backend_pixelflood
+import backend_ddp
 import frontend
 import random
 import time
 
 
-canvas = frontend.frontend(backend_pixelflood.backend_pixelflood('192.168.65.140', 1338, False, (64, 32)))
+canvas = frontend.frontend(backend_ddp.backend_ddp('192.168.65.140', 4048, (64, 32)))
 canvas.add_animation('some_message', frontend.scroll_text(canvas, 'white', 'Hello, world!'))
 
 while True:
