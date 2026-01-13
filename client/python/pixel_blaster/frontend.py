6289903d169e6ded7c12bdb294e2459294283e05
Folkert van Heusden <folkert@vanheusden.com>
2026-01-13 07:37:36 +0100

    color_name_to_rgb_alpha

--- client/python/pixel_blaster/frontend.py
+++ client/python/pixel_blaster/frontend.py
@@ -41,15 +41,22 @@ class frontend:
     def clear_screen(self) -> None:
         self.b.clear_screen()
 
-    def color_name_to_rgb(self, color: str) -> list[int, int, int, int]:
+    def color_name_to_rgb_alpha(self, color: str) -> list[int, int, int, int]:
         try:
             r, g, b, a = colors.to_rgba(color)
             return int(r * 255), int(g * 255), int(b * 255), int(a * 255)
         except ValueError as v:
             return 127, 127, 127, 255
 
+    def color_name_to_rgb(self, color: str) -> list[int, int, int]:
+        try:
+            r, g, b, a = colors.to_rgba(color)
+            return int(r * 255), int(g * 255), int(b * 255)
+        except ValueError as v:
+            return 127, 127, 127
+
     def set_pixel_color_by_name(self, x: int, y: int, color: str) -> None:
-        r, g, b, a = self.color_name_to_rgb(color)
+        r, g, b, a = self.color_name_to_rgb_alpha(color)
         self.b.set_pixel(x, y, r, g, b, a)
 
     # r/g/b: 0...255
@@ -84,7 +91,7 @@ class frontend:
                 error += dx
 
     def draw_line_color_by_name(self, x_start: int, y_start: int, x_end: int, y_end: int, color: str) -> None:
-        r, g, b, a = self.color_name_to_rgb(color)
+        r, g, b, a = self.color_name_to_rgb_alpha(color)
         self.draw_line_rgb(x_start, y_start, x_end, y_end, r, g, b, a)
 
     def draw_pil_Image(self, pil_canvas: Image, x_offset: int, y_offset: int):
@@ -104,7 +111,7 @@ class frontend:
         self.draw_pil_Image(image, x, y)
 
     def draw_text_color_by_name(self, x: int, y: int, font_name: str, font_height: float, text: str, color: str) -> None:
-        r, g, b, a = self.color_name_to_rgb(color)
+        r, g, b, a = self.color_name_to_rgb_alpha(color)
         self.draw_text(x, y, font_name, font_height, text, r, g, b, a)
 
     def draw_sparkline_rgb(self, x: int, y: int, height: int, values: list[float], r: int, g: int, b: int) -> None:
@@ -119,7 +126,7 @@ class frontend:
                 y_prev_offset = y_offset
 
     def draw_sparkline_color_by_name(self, x: int, y: int, height: int, values: list[float], color: str) -> None:
-        r, g, b, a = self.color_name_to_rgb(color)
+        r, g, b, a = self.color_name_to_rgb_alpha(color)
         self.draw_sparkline_rgb(x, y, height, values, r, g, b, a)
 
     def fill_region_rgb(self, x: int, y: int, width: int, height: int, r: int, g: int, b: int) -> None:
@@ -128,7 +135,7 @@ class frontend:
                 self.b.set_pixel(work_x, work_y, r, g, b)
 
     def fill_region_color_by_name(self, x: int, y: int, width: int, height: int, color: str) -> None:
-        r, g, b, a = self.color_name_to_rgb(color)
+        r, g, b, a = self.color_name_to_rgb_alpha(color)
         self.fill_region_rgb(x, y, width, height, r, g, b, a)
 
     def send_to_screen(self):
@@ -154,7 +161,7 @@ class scroll_text(animation):
         self.image = Image.new('RGBA', (text_dimensions[2], text_dimensions[3]))
         self.text_width = text_dimensions[2]
         pil_canvas = ImageDraw.Draw(self.image)
-        pil_canvas.text((0, 0), text, f.color_name_to_rgb(color_name), font = font)
+        pil_canvas.text((0, 0), text, f.color_name_to_rgb_alpha(color_name), font = font)
 
     def tick(self, f: frontend) -> None:
         if self.x == None:
