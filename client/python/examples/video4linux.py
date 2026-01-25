edcb99e291852f9bce691761ee6133563d06c720
Folkert van Heusden <folkert@vanheusden.com>
2026-01-25 10:52:48 +0100

    bouncing camera

--- client/python/examples/video4linux.py
+++ client/python/examples/video4linux.py
@@ -14,12 +14,17 @@ def decode_fourcc(cc):
     return "".join([chr((int(cc) >> 8 * i) & 0xFF) for i in range(4)])
 
 
-b = pixel_yeeter.backend_ddp.backend_ddp('192.168.65.140', 4048, (64, 32))
-width, height = b.get_width(), b.get_height()
+canvas = pixel_yeeter.frontend.frontend(pixel_yeeter.backend_ddp.backend_ddp('192.168.65.140', 4048, (64, 32)))
+width, height = canvas.get_resolution()
 
 cap = cv2.VideoCapture(0)
 cam_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 cam_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+x = 0
+y = 0
+dx = -1
+dy = 1
 
 while True:
     _status, img = cap.read()
@@ -37,16 +42,27 @@ while True:
             print("unsupported video format")
             break
 
-    factor_x = cam_width / width
-    factor_y = cam_height / height
+    factor_x = cam_width / (width * 2)
+    factor_y = cam_height / (height * 2)
     factor = max(factor_x, factor_y)
 
     new_width = int(cam_width / factor)
     new_height = int(cam_height / factor)
 
     img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
-    for y in range(new_height):
-        for x in range(new_width):
-            p = img[y][x]
-            b.set_pixel(x, y, int(p[2]), int(p[1]), int(p[0]))
-    b.update()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
+    pil_img = Image.fromarray(img)
+    canvas.clear_front()
+    canvas.draw_pil_Image(pil_img, x, y, pixel_yeeter.backend.layer_types.front)
+    canvas.send_to_screen()
+
+    x += dx
+    if x <= -new_width / 4:
+        dx = 1
+    elif x >= 0:
+        dx = -1
+    y += dy
+    if y <= -new_height / 2:
+        dy = 1
+    elif y >= 0:
+        dy = -1
