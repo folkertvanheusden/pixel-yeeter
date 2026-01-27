4a00a9982e444bea211a40e605ced069a6994e8e
Folkert van Heusden <folkert@vanheusden.com>
2026-01-27 12:48:15 +0100

    blob

--- client/python/examples/video4linux.py
+++ client/python/examples/video4linux.py
@@ -22,13 +22,16 @@ cap = cv2.VideoCapture(0)
 cam_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
 cam_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-mode = 0
 mode_since = time.time()
+blob = True
 
 bx = 0
 by = 0
 dx = -1
 dy = 1
+
+xt = 0
+yt = 0
 
 while True:
     _status, img = cap.read()
@@ -46,58 +49,53 @@ while True:
             print(f'{fourcc}: unsupported video format')
             break
 
-    if mode == 0:
-        factor_x = cam_width / (width * 2)
-        factor_y = cam_height / (height * 2)
-        factor = max(factor_x, factor_y)
+    factor_x = cam_width / (width * 2)
+    factor_y = cam_height / (height * 2)
+    factor = max(factor_x, factor_y)
 
-        new_width = int(cam_width / factor)
-        new_height = int(cam_height / factor)
+    new_width = int(cam_width / factor)
+    new_height = int(cam_height / factor)
 
-        img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
-        pil_img = Image.fromarray(img)
-        canvas.clear_front()
-        canvas.draw_pil_Image(pil_img, bx, by, pixel_yeeter.backend.layer_types.front)
-        canvas.send_to_screen()
+    img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
+    pil_img = Image.fromarray(img)
 
-        bx += dx
-        if bx <= -new_width / 4:
-            dx = 1
-        elif bx >= 0:
-            dx = -1
-        by += dy
-        if by <= -new_height / 2:
-            dy = 1
-        elif by >= 0:
-            dy = -1
+    # blob
+    new_img = Image.new(mode='RGBA', size=(new_width, new_height))
 
-    else:
-        img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGBA)
-        pil_img = Image.fromarray(img)
-        new_img = Image.new(mode='RGBA', size=(width, height))
+    for y in range(new_height):
+        for x in range(new_width):
+            gx = x + math.sin((x + xt) / new_width * 2 * 3.1415) * new_width / 8
+            if gx < 0:
+                gx = 0
+            elif gx >= new_width:
+                gx = new_width -1
+            gy = y + math.sin((y + yt) / new_height * 2 * 3.1415) * new_height / 8
+            if gy < 0:
+                gy = 0
+            elif gy >= new_height:
+                gy = new_height -1
+            rgba = pil_img.getpixel((int(gx), int(gy)))
+            new_img.putpixel((x, y), rgba)
 
-        for y in range(height):
-            for x in range(width):
-                gx = x + math.sin(x / width * 2 * 3.1415) * width / 8
-                if gx < 0:
-                    gx = 0
-                elif gx >= width:
-                    gx = width -1
-                gy = y + math.sin(y / height * 2 * 3.1415) * height / 8
-                if gy < 0:
-                    gy = 0
-                elif gy >= height:
-                    gy = height -1
-                rgba = pil_img.getpixel((int(gx), int(gy)))
-                new_img.putpixel((x, y), rgba)
+    xt += 1
+    if xt >= new_width:
+        xt = 0
+    yt += 1
+    if yt >= new_height:
+        yt = 0
 
-        canvas.clear_front()
-        canvas.draw_pil_Image(new_img, 0, 0, pixel_yeeter.backend.layer_types.front)
-        canvas.send_to_screen()
+    canvas.clear_front()
+    canvas.draw_pil_Image(new_img, bx, by, pixel_yeeter.backend.layer_types.front)
+    canvas.send_to_screen()
 
-    now = time.time()
-    if now - mode_since >= 7.5:
-        mode_since = now
-        mode = (mode + 1) % 2
+    bx += dx
+    if bx <= -new_width / 4:
+        dx = 1
+    elif bx >= 0:
+        dx = -1
+    by += dy
+    if by <= -new_height / 2:
+        dy = 1
+    elif by >= 0:
+        dy = -1
