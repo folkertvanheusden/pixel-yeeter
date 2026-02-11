bc714377235aab2bad815ccd78f19280849fbd00
Folkert van Heusden <folkert@vanheusden.com>
2026-02-11 06:59:01 +0100

    replaced LANCZOS by BICUBIC as LANCZOS gives artifacts

--- client/python/pixel_yeeter/frontend.py
+++ client/python/pixel_yeeter/frontend.py
@@ -223,7 +223,7 @@ class scroll_text(animation):
         new_height = f.get_resolution()[1]
         if new_height != prepared_text[2]:
             new_width  = int(new_height * prepared_text[1] / prepared_text[2])
-            self.image = self.image.resize((new_width, new_height), Image.LANCZOS)
+            self.image = self.image.resize((new_width, new_height), Image.BICUBIC)
             self.text_width = new_width
         else:
             self.text_width = prepared_text[1]
